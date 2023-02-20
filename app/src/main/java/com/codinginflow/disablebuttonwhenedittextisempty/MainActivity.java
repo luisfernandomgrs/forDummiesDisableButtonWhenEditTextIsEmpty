098e87ebbcbe2a221414e8cdac580ce1681769ef
Luis Fernando Machado <>
2023-02-20 17:05:30 -0300

    first commit

---
+++ app/src/main/java/com/codinginflow/disablebuttonwhenedittextisempty/MainActivity.java
@@ -0,0 +1,14 @@
+package com.codinginflow.disablebuttonwhenedittextisempty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
